b723cb81b8dd5154a7b131f009e6ea4e1e30b967
Windmill-City <1449182174@qq.com>
2021-08-06 17:34:58 +0800

    Comment fix comment for IInputContext.kt
    Signed-off-by: Windmill-City <1449182174@qq.com>

--- src/commonMain/kotlin/ingameIME/context/IInputContext.kt
+++ src/commonMain/kotlin/ingameIME/context/IInputContext.kt
@@ -28,13 +28,13 @@ interface IInputContext {
     /**
      * Current active language profile of the context
      *
+     * @usage Assign a [ILanguageProfile] to change the active one
      * Normally, we don't need to change active language, but display it on screen
-     * User can use hotkey to change this themselves
      *
      * As the input method(IM) always associate with the language, we just need to display
      * active language when there is no active IM
      *
-     * May change by operating-system
+     * User can use system specified hotkey to change this
      * Observable value
      * @see ingameIME.utils.setCallback
      */
@@ -43,20 +43,20 @@ interface IInputContext {
     /**
      * Current active input method of the context
      *
-     * Normally, we don't need to change active input method(IM), but display what IM is on screen,
-     * like the conversion mode popup
+     * @usage Assign a [IInputMethodProfile] to change the active one
+     * Normally, we don't need to change active input method(IM),
+     * but display what IM is on screen, like the conversion mode popup
      *
-     * User can use hotkey to change this themselves
-     *
-     * May change by operating-system
+     * User can use system specified hotkey to change this
      * Observable value
      * @see ingameIME.utils.setCallback
      */
     var inputMethod: IInputMethodProfile
 
     /**
-     * Current [IInputState] of the context, set if input method is enabled
+     * Current [IIMState] of the context, set if input method is enabled
      *
+     * @usage Assign a [IIMState] to change the active one
      * @see [ingameIME.context.inputState.IAllowIM]
      * @see [ingameIME.context.inputState.IForbidIM]
      *
@@ -68,13 +68,11 @@ interface IInputContext {
     /**
      * Current [IConversionMode] of the context
      *
+     * @usage Assign a [IConversionMode] to change the active one
      * Normally, we don't need to change conversion mode, but just render this state on screen
      * User can use input method(IM)'s hotkey to change this mode themselves
      *
      * When IM is enabled, we need to show a popup(last for 3-5 seconds) to tell user what conversion mode is
-     *
-     * @see [ingameIME.context.inputState.inputMode.conversion.IAlphaNumericMode]
-     * @see [ingameIME.context.inputState.inputMode.conversion.INativeMode]
      *
      * Should call [IInputState.onApplyState] or [IInputState.onLeaveState] when changing
      * @see [ingameIME.context.inputState.stateOf]
@@ -87,6 +85,8 @@ interface IInputContext {
 
     /**
      * Current [ISentenceMode] of the context
+     *
+     * @usage Assign a [ISentenceMode] to change the active one
      *
      * Should call [IInputState.onApplyState] or [IInputState.onLeaveState] when changing
      * @see [ingameIME.context.inputState.stateOf]
@@ -106,11 +106,10 @@ interface IInputContext {
      * If in UI-Less Mode
      *
      * When set to true, input method(IM)'s
-     * [Composition] Window & [ingameIME.context.composition.CandidateList] Window will not draw,
-     * so we need to draw it ourselves
+     * [Composition] Window & [ingameIME.context.composition.CandidateList] Window will not draw
      *
-     * @Note this should be true when the game is in full screen mode, as we directly write to the frame buffer,
-     * it is impossible to show another window on the screen at the same time
+     * @Note this should be true when the game is in full screen mode,
+     * it is impossible to show another window on the screen as we directly write to the frame buffer
      *
      * @Note this value can be changed on the fly
      */
